66c40a7144f4e7bdad2957e087a5e07b1bcddd60
Aldiyar <aldikzhaks@gmail.com>
2025-09-25 22:00:21 +0500

    init

---
+++ src/main/java/org/example/authorizationapp/AuthorizationAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.authorizationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/authorizationapp/AuthorizationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.authorizationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
